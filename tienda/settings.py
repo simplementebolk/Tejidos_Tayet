7dbad700af540352719e12d0c167a45a8316f215
Enzo Rodrigo Araya Venegas <137220964+simplementebolk@users.noreply.github.com>
2026-08-13 23:22:26 -0400

    Add STATIC_ROOT for static file collection

--- tienda/settings.py
+++ tienda/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_URL = '/acceso_denegado/'
 
 # Default primary key field type
